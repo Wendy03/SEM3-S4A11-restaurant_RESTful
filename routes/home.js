74aeba2b19702b17bfe9e9d007cd983a185546e8
wendy03 <wendysu03@gmail.com>
2019-11-15 19:39:49 +0800

    Update routes & search

--- routes/home.js
+++ routes/home.js
@@ -10,4 +10,54 @@ router.get('/', (req, res) => {
   })
 })
 
+//Search Restaurant
+router.get('/search', (req, res) => {
+  Restaurant.find((err, restaurants) => {
+    const keyword = req.query.keyword
+    if (err) return console.error(err)
+    const searchResult = restaurants.filter(restaurant => {
+      return (
+        restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+        restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) ||
+        restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+      )
+    })
+    let emptyDate = searchResult.length === 0 ? true : false
+
+    res.render('index', { style: 'index.css', restaurants: searchResult, keyword, emptyDate })
+  })
+})
+
+// sort restaurant
+router.get('/:filter', (req, res) => {
+  let sort
+  switch (req.params.filter) {
+    case 'name-asc':
+      sort = { name_en: 'asc' }
+      break;
+    case 'name-desc':
+      sort = { name_en: 'desc' }
+      break;
+    case 'category-asc':
+      sort = { category: 'asc' }
+      break;
+    case 'rating-asc':
+      sort = { rating: -1 }
+      break;
+    case 'rating-desc':
+      sort = { rating: 1 }
+      break;
+    case 'location-asc':
+      sort = { location: 'asc' }
+      break;
+  }
+  Restaurant.find({})
+    .sort(sort)
+    .exec((err, restaurants) => {
+      if (err) return console.error(err)
+      return res.render('index', { style: 'index.css', restaurants })
+    })
+})
+
+
 module.exports = router
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -7,53 +7,6 @@ router.get('/', (req, res) => {
   return res.redirect('/')
 })
 
-//Search Restaurant
-router.get('/search', (req, res) => {
-  Restaurant.find((err, restaurants) => {
-    const keyword = req.query.keyword
-    if (err) return console.error(err)
-    const searchResult = restaurants.filter(restaurant => {
-      return (
-        restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-        restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) ||
-        restaurant.category.toLowerCase().includes(keyword.toLowerCase())
-      )
-    })
-    res.render('index', { style: 'index.css', restaurants: searchResult, keyword })
-  })
-})
-
-// sort restaurant
-router.get('/sort/:filter', (req, res) => {
-  let sort
-  switch (req.params.filter) {
-    case 'name-asc':
-      sort = { name_en: 'asc' }
-      break;
-    case 'name-desc':
-      sort = { name_en: 'desc' }
-      break;
-    case 'category-asc':
-      sort = { category: 'asc' }
-      break;
-    case 'rating-asc':
-      sort = { rating: -1 }
-      break;
-    case 'rating-desc':
-      sort = { rating: 1 }
-      break;
-    case 'location-asc':
-      sort = { location: 'asc' }
-      break;
-  }
-  Restaurant.find({})
-    .sort(sort)
-    .exec((err, restaurants) => {
-      if (err) return console.error(err)
-      return res.render('index', { style: 'index.css', restaurants })
-    })
-})
-
 // 新增一筆 restaurant 頁面
 router.get('/new', (req, res) => {
   return res.render('new', { style: 'form.css' })
